7966c52f2f6807f45e251aec07d88e69564ef979
paul6ix <okporp@gmail.com>
2021-05-17 18:08:23 +0100

    Heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn WebScrapingApp.wsgi
--- WebScrapingApp/settings.py
+++ WebScrapingApp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-eiy9w9ljt6+6@@-_@durknf3avw=va&=&p87-xcwsddx()t%+h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://scraper-6ix.herokuapp.com/']
 
 
 # Application definition
@@ -121,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
